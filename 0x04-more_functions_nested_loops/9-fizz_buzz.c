305a6168088539b8443afad2acde38dd24d39a32
KabeloMak <kabelo.makhetha17@gmail.com>
2023-06-23 15:23:56 -0700

    push

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,20 +13,22 @@ int main(void)
 
 {
 	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
-
 	{
-		if (i % 15 == 0)
-			printf("Fizzbuzz");
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf("%s ", f);
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("%s ", b);
 		else
-			printf("%i", i);
-		if (i < 100)
-			printf(" ");
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
