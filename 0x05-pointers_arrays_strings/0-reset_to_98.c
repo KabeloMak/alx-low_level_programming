73d077154ea6dbb9f8f643c8509955b6516ff932
KabeloMak <kabelo.makhetha17@gmail.com>
2023-06-27 08:31:14 -0700

    send

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function takes pointer to to an int as a parameter and updates the valyue uit points to 98
+ * reset_to_98 - function takes pointer to to an o 98
  * @n: - the vakye to be updated to 98
  * Return: Always 0
  */
 
 void reset_to_98(int *n)
-
 {
 	*n = 98;
 }
